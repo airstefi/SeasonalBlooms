f82452767a14cada011ba303e8e8460df641ff74
Stefani Gancheva <126952489+airstefi@users.noreply.github.com>
2023-12-22 01:03:21 +0200

    Update PlantandVeggies.js

--- src/PlantandVeggies.js
+++ src/PlantandVeggies.js
@@ -1,4 +1,3 @@
-// src/components/FruitInfo.js
 import React from "react";
 import { products } from "./veggieinfo.json";
 import "./styles.css";
@@ -8,12 +7,13 @@ const PlantandVeggies = () => {
     <div>
       <h2>Plants & Veggies Page</h2>
       <ul>
+        {/* this is a map function where we get the items from the json file and print them to the screen
+        in our plants and veggiees page */}
         {products.map((product) => (
           <li key={product.pid}>
             <h3>{product.name}</h3>
             <p>Harvest Month: {product.harvest}</p>
             <p>Plant Month: {product.plant}</p>
-            {/* Add other details as needed */}
           </li>
         ))}
       </ul>
