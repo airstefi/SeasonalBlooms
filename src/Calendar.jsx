e4488f4fa0cb8a5e1c33aa569db189dbc4dd502d
Stefani Gancheva <126952489+airstefi@users.noreply.github.com>
2023-12-22 01:02:29 +0200

    Update Calendar.jsx

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,12 +1,13 @@
-// src/components/Calendar.js
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./styles.css";
 
+//manage the selected date
 const Calendar = ({ onDateChange }) => {
   const [selectedDate, setSelectedDate] = useState(null);
 
+  //handles the change of date by updating the selected date and notifying the parent of the change
   const handleDateChange = (date) => {
     setSelectedDate(date);
     onDateChange(date);
