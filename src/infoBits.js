cc097ab9b2f671db82db07de5e6ccc4d0e11496b
eibaa <151152150+eibaa@users.noreply.github.com>
2023-12-21 11:16:19 +0000

    Create infoBits.js

---
+++ src/infoBits.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const info = [
+  {
+    title: "Bees and gardens",
+    bit: "- This will be a fact sheet about bees. Preferably, it will only show up when the seasons suit it. Maybe in the end I could include some photos or links for further investigation",
+  },
+  {
+    title: "Your impact on the environment",
+    bit: "- This could be something about the benefits your garden could have on the environment.",
+  },
+  {
+    title: "Pests in the garden",
+    bit: "- Include a section on garden pests and how to manage them",
+  },
+];
+
+export { info };
