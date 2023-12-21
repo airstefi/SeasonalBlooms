2f39d5d3b24b5bce7e783a61c7b19182c642e2a3
Stefani Gancheva <126952489+airstefi@users.noreply.github.com>
2023-12-21 14:49:57 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,53 @@
 import React, { useState } from "react";
 import Calendar from "./Calendar";
 import FruitInfo from "./FruitsInfo";
+import PlantandVeggies from "./PlantandVeggies";
 import "./styles.css";
 
 const App = () => {
   const [selectedDate, setSelectedDate] = useState(null);
+  const [activePage, setActivePage] = useState("home");
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
 
+  const handlePageChange = (page) => {
+    setActivePage(page);
+  };
+
   return (
     <div>
+      <header>
+        <ul>
+          <li>
+            <a
+              href="#"
+              className={activePage === "home" ? "active" : ""}
+              onClick={() => handlePageChange("home")}
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className={activePage === "plantsAndVeggies" ? "active" : ""}
+              onClick={() => handlePageChange("plantsAndVeggies")}
+            >
+              Plants & Veggies
+            </a>
+          </li>
+        </ul>
+      </header>
       <h1>Seasonal Blooms</h1>
-      <p>please choose a date</p>
-      <Calendar onDateChange={handleDateChange} />
-      {selectedDate && <FruitInfo date={selectedDate} />}
+      {activePage === "home" && <p>Please choose a date</p>}
+      {activePage === "home" && <Calendar onDateChange={handleDateChange} />}
+      {selectedDate && activePage === "home" && (
+        <FruitInfo date={selectedDate} />
+      )}
+
+      {activePage === "plantsAndVeggies" && <PlantandVeggies />}
     </div>
   );
 };
