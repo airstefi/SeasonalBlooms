d18d52fe77d296a35bdd93fc71d6ae4174c37b98
eibaa <151152150+eibaa@users.noreply.github.com>
2023-12-21 18:20:32 +0000

    Update App.jsx
    Added infoBits and totd.
    One issue - can't figure out how to mape the map function work in the seperate function, and I can't get it to print only in the home page like all the rest of the lines
    It's also in its og place in the App return but doesn't work above and prints in both home and fruits and veggies if down there.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import React, { useState } from "react";
 import Calendar from "./Calendar";
 import FruitInfo from "./FruitsInfo";
 import PlantandVeggies from "./PlantandVeggies";
+import { tip } from "./tips/totd";
+import { info } from "./tips/infoBits";
 import "./styles.css";
 
 const App = () => {
   const [selectedDate, setSelectedDate] = useState(null);
   const [activePage, setActivePage] = useState("home");
+  const [tipRand, setTipRand] = useState(0); //for tip function
+
+  function changeTipRand(i) {
+    setTipRand(i + 1);
+  }
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
@@ -16,6 +23,28 @@ const App = () => {
   const handlePageChange = (page) => {
     setActivePage(page);
   };
+
+  function Robert(props) {
+    //num is made to be a pos random number within array length
+    var num = Math.floor(Math.random() * 3);
+    return (
+      <>
+        <h1> </h1>
+        <button onClick={() => props.changeAFromParent(num)}>New Tip</button>
+      </>
+    );
+  }
+
+  function printDidYK(props) {
+    return (
+      {info.map((c, index) => (
+        <p key={index}>
+          {" "}
+          <h3> {c.title} </h3> {c.bit}
+        </p>
+      ))}
+    );
+  }
 
   return (
     <div>
@@ -47,7 +76,20 @@ const App = () => {
       {selectedDate && activePage === "home" && (
         <FruitInfo date={selectedDate} />
       )}
-
+      {activePage === "home" && (
+        <Robert aFromParent={tipRand} changeAFromParent={changeTipRand} />
+      )}
+      {activePage === "home" && <h4> {tip[tipRand].content} </h4>}
+      {activePage === "home" && <hr />}
+      {activePage === <h2> Did you know... </h2>}
+      {activePage === "home" && <printDidYK bFromParent={infoBits}/> }
+      
+      {info.map((c, index) => (
+        <p key={index}>
+          {" "}
+          <h3> {c.title} </h3> {c.bit}
+        </p>
+      ))}
       {activePage === "plantsAndVeggies" && <PlantandVeggies />}
     </div>
   );
