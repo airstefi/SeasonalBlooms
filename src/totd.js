9489d93f5fc595e4e93c672360fc80310112d280
eibaa <151152150+eibaa@users.noreply.github.com>
2023-12-21 11:16:43 +0000

    Create totd.js

---
+++ src/totd.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const tip = [
+  {
+    id: 1,
+    content: "Click the 'New Tip' button for some helpful or random facts!",
+  },
+  {
+    id: 2,
+    content:
+      "There are a few informational pieces at the bottom of the page with further links for learning about your garden and what you can do for the environment.",
+  },
+  {
+    id: 3,
+    content:
+      "It is important to plan your garden before planting - some plants are greedy with soil and will end up taking over your whole garden!",
+  },
+  {
+    id: 4,
+    content:
+      "While wildlife within the garden is to be expected, bugs and pests can harm the enviroment you create! Learn more below in the 'Did you know' section!",
+  },
+];
+
+export { tip };
