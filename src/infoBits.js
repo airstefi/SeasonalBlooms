0f4b13d468abc668497bd5950458f2856b23c9eb
eibaa <151152150+eibaa@users.noreply.github.com>
2023-12-22 10:33:30 +0000

    Update infoBits.js
    Fleshed out info bits

--- src/infoBits.js
+++ src/infoBits.js
@@ -1,23 +1,17 @@
 import React, { useState } from "react";
 
-/*
-  This file contains a database of extra pieces of information to support the daily garden forecast.
-  Included are things such as information of animals commonly found in gardens - i.e. bugs, bees, birds - and how
-  to deal / care for them appropriately to benefit the environment.
-  Could also just include links to resources for further learning in gardening and the environment.
-*/
 const info = [
   {
     title: "Bees and gardens",
-    bit: "- This will be a fact sheet about bees. Preferably, it will only show up when the seasons suit it. Maybe in the end I could include some photos or links for further investigation",
+    bit: "Apart from being expert producers of honey, bees also play a key role in pollination of nearly all flowers and fruits we enjoy - meaning the absence of bees could be the very thing holding your garden back. Bees spread pollen from flower to flower - a process called pollination, which is critical for plants to reproduce, produce seeds, and keep flowers and fruits coming in the months ahead. Without pollinators, flowers will not produce as many seeds or as many flowers. If you want flowers or fruits in your garden, you’re going to need some kind of pollinator. Given their unique place in the ecosystem (as well as their current struggle keeping colonies healthy and thriving), bees might just be your best bet.",
   },
   {
     title: "Your impact on the environment",
-    bit: "- This could be something about the benefits your garden could have on the environment.",
+    bit: "Gardening is a way of improving the environment we live in via the addition of plants, flowers, fruits, and vegetables. When we garden — especially organically — we improve the air and the soil quality. We are growing our own food, beautifying our space, and instead of taking something away from the natural world, we are adding and tending to something green and beautiful. Considering how deep humanity’s carbon footprint is these days, something that takes away CO2 and harmful chemicals could definitely be considered helpful for the environment.",
   },
   {
     title: "Pests in the garden",
-    bit: "- Include a section on garden pests and how to manage them",
+    bit: "Most insects and wildlife found in the vegetable garden are not pests, around 95% are beneficial or harmless. The best gardens are teeming with life, and maintaining the right balance of beneficial pests and wildlife while reducing or eliminating the populations of damage causing insects and pests is what pest control is all about. The easiest and most cost effective method of controlling pests is to use natural and preventative means and there are a few that work for every pest. For some tips, visit https://www.quickcrop.ie/blog/garden-pests/.",
   },
 ];
 
